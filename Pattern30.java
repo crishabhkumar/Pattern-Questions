73aa043fda048741317b6c48d94308b5a4c83877
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-23 22:07:47 +0530

    Create Pattern30.java

---
+++ Pattern30.java
@@ -0,0 +1,25 @@
+/*
+30.         1
+          2 1 2
+        3 2 1 2 3
+      4 3 2 1 2 3 4
+    5 4 3 2 1 2 3 4 5
+    
+    */
+
+
+
+static void pattern30(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
