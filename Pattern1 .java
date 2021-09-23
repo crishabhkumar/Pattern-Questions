11884da2346ff241bc8e224289c868d52196b510
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-23 18:00:15 +0530

    Create Pattern1 .java

---
+++ Pattern1 .java
@@ -0,0 +1,23 @@
+/*
+1.  *****
+    *****
+    *****
+    *****
+    *****
+
+ */
+
+
+static void pattern1(int n){
+        for (int row = 0; row < n; row++) {
+            //for every row, run the col
+            for (int col = 0;col <= n;col++){
+                System.out.print("* ");
+            }
+            //when one row is printed we need to print new line
+            System.out.println();
+
+        }
+
+    }
+    
